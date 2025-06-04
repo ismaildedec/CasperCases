4a9c15d077106ccf8a02c8cd72059a0d6fbd2110
ismaildedec <ddcismail@gmail.com>
2025-06-04 03:28:34 +0300

    Created Product model with XML documentation

---
+++ Case01/ProductApi/Models/Product.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ProductApi.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Category { get; set; } = string.Empty;
+    }
+} 
