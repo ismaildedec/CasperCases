b8634ec5ca9faef41f2a3be066e261949f979117
ismaildedec <ddcismail@gmail.com>
2025-06-04 03:38:51 +0300

    Configured DI for ProductService, Swagger settings and CORS policy

--- Case01/ProductApi/Program.cs
+++ Case01/ProductApi/Program.cs
@@ -1,22 +1,57 @@
+#region Using
+using Microsoft.OpenApi.Models;
+using ProductApi.Services;
+#endregion
+
 var builder = WebApplication.CreateBuilder(args);
 
+#region Services
 
 builder.Services.AddControllers();
 
+
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "Product API",
+        Version = "v1",
+        Description = "Mock ürün listeleme ve filtreleme servisi."
+    });
+});
+
+
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod();
+    });
+});
+
+builder.Services.AddScoped<IProductService, ProductService>();
+#endregion
 
 var app = builder.Build();
 
-
+#region Middleware
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
+app.UseCors();
 app.UseAuthorization();
-
 app.MapControllers();
+#endregion
+
+#region Startup Message
+Console.WriteLine("----------------------------------------");
+Console.WriteLine("Product API başlatıldı!");
+Console.WriteLine("Swagger UI: /swagger");
+Console.WriteLine("----------------------------------------");
+#endregion
 
 app.Run();
